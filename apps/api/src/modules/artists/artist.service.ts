12f237a3ede3fa09786cae3146bc00308d908c25
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-03-06 04:37:08 +0530

    implemented artist service layer

---
+++ apps/api/src/modules/artists/artist.service.ts
@@ -0,0 +1,36 @@
+import { Artist } from "@/generated/prisma";
+import { ArtistRepository } from "./artist.repository";
+import { CreateArtistInput } from "./schemas/create-artist.schema";
+
+
+
+export class ArtistService {
+    private repository: ArtistRepository;
+
+    constructor() {
+        this.repository = new ArtistRepository();
+    }
+
+
+    async getAllArtist(): Promise<Artist[]> {
+        return this.repository.findAll();
+    }
+
+    async getArtistById(id: string) {
+        const artist = await this.repository.findById(id);
+
+        if (!artist) {
+            throw new Error("Artist not found");
+        }
+
+        return artist;
+    }
+
+    async createArtist(data: CreateArtistInput): Promise<Artist> {
+        return this.repository.create(data);
+    }
+
+    async getArtistSongs(id: string) {
+        return this.repository.findSongByArtist(id);
+    }
+}
