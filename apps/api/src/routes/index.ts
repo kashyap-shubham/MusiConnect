7cde2031b7a93d70927ae16ae20b9cf8555a66e8
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-03-09 00:29:03 +0530

    added user routes to global routes

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import songRoutes from "../modules/songs/songs.routes";
-import artistRoutes from "../modules/artists/artists.routes";
-import albumRoutes from "../modules/albums/albums.routes";
-import playlistRoutes from "../modules/playlists/playlists.routes";
+import songRoutes from "@/modules/songs/songs.routes";
+import artistRoutes from "@/modules/artists/artists.routes";
+import albumRoutes from "@/modules/albums/albums.routes";
+import playlistRoutes from "@/modules/playlists/playlists.routes";
+import userRoutes from "@/modules/users/users.routes";
 
 const router: Router = Router();
 
@@ -11,5 +12,6 @@ router.use("/songs", songRoutes);
 router.use("/artists", artistRoutes);
 router.use("/albums", albumRoutes);
 router.use("/playlists", playlistRoutes);
+router.use("/users", userRoutes);
 
 export default router;
