1ae9195f5dd222a3ea964e6d4929ac342ac772fb
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-03-08 17:50:38 +0530

    added error handler middleware

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,8 +2,7 @@ import express, { type Application } from "express";
 import routes from "./routes";
 import { env } from "./config/env";
 import cors from "cors";
-
-
+import { errorHandler } from "./errors/errorHandler";
 
 const app: Application = express();
 
@@ -12,10 +11,12 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api", routes);
+
+app.use(errorHandler);
 
 const PORT = env.PORT;
 
