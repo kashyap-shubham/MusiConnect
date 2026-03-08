ec8d330f78748b4ec991e1e783c421e3b3a64d8d
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-03-08 16:28:03 +0530

    added global error handler

---
+++ apps/api/src/errors/errorHandler.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiError } from "./ApiErrors";
+
+export const errorHandler = (
+  err: Error | ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = "Internal Server Error";
+
+  if (err instanceof ApiError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
