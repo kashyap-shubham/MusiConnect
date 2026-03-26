30cf0d0bed6787389d2c945700e7b224d0ffb2a8
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-03-26 22:26:54 +0530

    added pictures of artists

--- apps/web/src/components/landing/Heading.tsx
+++ apps/web/src/components/landing/Heading.tsx
@@ -1,31 +1,91 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Heading() {
 
   return (
-    <section className="px-6 pt-10">
+    <section className="px-6 pt-10 relative">
 
       {/* floating cards */}
-      <div className="absolute inset-0 ">
+      <div className="absolute inset-0 pointer-events-none">
+
+        {/* left farthest */}
+        <div className="absolute left-[-8%] top-[82%] -rotate-60">
+          <div className="h-48 w-50 rounded-2xl overflow-hidden shadow-xl bg-linear-to-br from-green-500/40 to-emerald-500/40">
+            <Image
+              src="/artists/artist3.jpg"
+              alt="artist"
+              width={800}
+              height={800}
+              className="object-cover scale-95 w-full h-full"
+            />
+          </div>
+        </div>
 
         {/* left far */}
         <div className="absolute left-[4%] top-[45%] -rotate-40">
-          <div className="h-48 w-50 rounded-2xl bg-linear-to-br from-orange-500/50 to-pink-500/50" />
+          <div className="h-48 w-50 rounded-2xl overflow-hidden shadow-xl bg-linear-to-br from-orange-500/50 to-pink-500/50">
+            <Image
+              src="/artists/artist1.jpg"
+              alt="artist"
+              width={300}
+              height={300}
+              className="object-cover scale-96 w-full h-full"
+            />
+          </div>
         </div>
         
         {/* left near */}
         <div className="absolute left-[22%] top-[28%] -rotate-20">
-          <div className="h-48 w-50 rounded-2xl bg-white/90" />
+          <div className="h-48 w-50 rounded-2xl overflow-hidden shadow-xl bg-white/90">
+            <Image
+              src="/artists/artist5.jpg"
+              alt="artist"
+              width={300}
+              height={300}
+              className="object-cover scale-96 w-full h-full"
+            />
+          </div>
         </div>
 
         {/* right near */}
         <div className="absolute right-[22%] top-[28%] rotate-20">
-          <div className="h-48 w-50 rounded-2xl bg-white/90" />
+          <div className="h-48 w-50 rounded-2xl overflow-hidden shadow-xl bg-white/90">
+            <Image
+              src="/artists/artist6.jpg"
+              alt="artist"
+              width={300}
+              height={300}
+              className="object-cover scale-150 w-full h-full"
+            />
+          </div>
         </div>
 
         {/* right far */}
         <div className="absolute right-[4%] top-[45%] rotate-40">
-          <div className="h-48 w-50 rounded-2xl bg-linear-to-br from-green-500/40 to-emerald-500/40" />
+          <div className="h-48 w-50 rounded-2xl overflow-hidden shadow-xl bg-linear-to-br from-green-500/40 to-emerald-500/40">
+            <Image
+              src="/artists/artist2.jpg"
+              alt="artist"
+              width={800}
+              height={800}
+              className="object-cover scale-95 w-full h-full"
+            />
+          </div>
+        </div>
+
+        {/* right farthest */}
+        <div className="absolute right-[-8%] top-[82%] rotate-60">
+          <div className="h-48 w-50 rounded-2xl overflow-hidden shadow-xl bg-linear-to-br from-green-500/40 to-emerald-500/40">
+            <Image
+              src="/artists/artist4.jpg"
+              alt="artist"
+              width={800}
+              height={800}
+              className="object-cover scale-95 w-full h-full"
+            />
+          </div>
         </div>
       </div>
 
@@ -36,7 +96,15 @@ export default function Heading() {
         {/* center big card */}
         <div className="relative mb-16 rounded-[28px] bg-[#d9d2e9] p-3 shadow-[0_40px_120px_rgba(0,0,0,0.8)]">
 
-          <div className="h-65 w-70 rounded-2xl bg-white/40" />
+          <div className="h-65 w-70 rounded-2xl overflow-hidden relative" >
+            <Image 
+              src="/artists/main.jpg" 
+              alt="artist" 
+              width={800} 
+              height={600} 
+              className="object-cover h-full w-full"
+            />
+          </div>
 
           <div className="absolute -right-10 top-[40%] rounded-xl bg-black/60 px-3 py-2 text-xs backdrop-blur">
             JPG
@@ -53,7 +121,7 @@ export default function Heading() {
         </div>
 
         {/* heading */}
-        <h1 className="text-[64px] font-semibold tracking-tight leading-[1.05] md:text-[88px]">
+        <h1 className="mt-2 text-[64px] font-semibold tracking-tight leading-[1.05] md:text-[88px]">
           Music&apos;s <br />
           Social Media
         </h1>
