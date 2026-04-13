9a8818a503ba85f760c3bfb7f272821915dcdcbf
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-04-13 22:51:02 +0530

    refactor(artists): add existence validation and implement update/delete service methods

--- apps/api/src/modules/artists/artists.service.ts
+++ apps/api/src/modules/artists/artists.service.ts
@@ -1,40 +1,75 @@
-import { Artist } from "../../generated/prisma";
+import { ApiError } from "@/errors/ApiError";
 import { ArtistRepository } from "./artists.repository";
 import { CreateArtistInput } from "./schemas/create-artist.schema";
+import { UpdateArtistInput } from "./schemas/update-artist.schema";
 
 
 
 export class ArtistService {
-    private repository: ArtistRepository;
+    private artistRepository: ArtistRepository;
 
     constructor() {
-        this.repository = new ArtistRepository();
+        this.artistRepository = new ArtistRepository();
     }
 
 
-    async getAllArtist(): Promise<Artist[]> {
-        return this.repository.findAll();
+    async getAllArtist() {
+        return this.artistRepository.findAll();
     }
 
     async getArtistById(id: string) {
-        const artist = await this.repository.findById(id);
+        const artist = await this.artistRepository.findById(id);
 
         if (!artist) {
-            throw new Error("Artist not found");
+            throw new ApiError(404, "Artist not found");
         }
 
         return artist;
     }
 
-    async createArtist(data: CreateArtistInput): Promise<Artist> {
-        return this.repository.create(data);
+    async createArtist(data: CreateArtistInput) {
+        return this.artistRepository.create(data);
+    }
+
+    async updateArtist(id: string, data: UpdateArtistInput) {
+        const artist = await this.artistRepository.findById(id);
+
+        if (!artist) {
+            throw new ApiError(404, "Artist not found");
+        }
+
+        return this.artistRepository.update(id, data);
+    }
+
+    async deleteArtist(id: string) {
+        const artist = await this.artistRepository.findById(id);
+
+        if (!artist) {
+            throw new ApiError(404, "Artist not found");
+        }
+
+        return this.artistRepository.delete(id);
     }
 
     async getArtistSongs(id: string) {
-        return this.repository.findSongByArtist(id);
+        const artist = await this.artistRepository.findById(id);
+
+        if (!artist) {
+            throw new ApiError(404, "Artist not found");
+        }
+
+        return this.artistRepository.findSongByArtist(id);
     }
 
     async getAlbumsByArtistId(artistId: string) {
-      return this.repository.findAlbumsByArtistId(artistId);
-    }
+
+        const artist = await this.artistRepository.findById(artistId);
+
+        if (!artist) {
+            throw new ApiError(404, "Artist not found");
+        }
+
+        return this.artistRepository.findAlbumsByArtistId(artistId);
+
+  }
 }
