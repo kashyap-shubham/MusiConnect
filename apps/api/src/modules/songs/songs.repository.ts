4fee7a743af2362e564976b5aaeb3e09c50aae4e
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-04-14 13:02:29 +0530

    feat(songs): added paginated method to fetch songs in repository

--- apps/api/src/modules/songs/songs.repository.ts
+++ apps/api/src/modules/songs/songs.repository.ts
@@ -153,4 +153,51 @@ export class SongRepository {
 
   }
 
+
+  async findAllPaginated(skip: number, take: number) {
+    const [songs, total] = await Promise.all([
+      prisma.song.findMany({
+        skip,
+        take,
+        orderBy: {
+          createdAt: "desc"
+        },
+
+        select: {
+          id: true,
+          title: true,
+          duration: true,
+          imageKey: true,
+          audioKey: true,
+
+          album: {
+            select: {
+              id: true,
+              title: true,
+              imageKey: true
+            }
+          },
+
+          artists: {
+            select: {
+              artist: {
+                select: {
+                  id: true,
+                  name: true
+                }
+              }
+            }
+          }
+        }
+      }),
+      prisma.song.count()
+    ]);
+
+    return {
+      data: songs,
+      total
+    };
+  
+  }
+
 }
