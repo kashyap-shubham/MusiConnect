e134d1a0bcdca6e79c2f8676696a45674ccc4ad9
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-04-17 04:11:27 +0530

    feat(auth): implemented maximum device per user

--- apps/api/src/auth/auth.routes.ts
+++ apps/api/src/auth/auth.routes.ts
@@ -8,54 +8,105 @@ import { prisma } from "@/lib/prisma";
 
 const authRouter:Router = Router();
 
+// limit no of devices per user
+const MAX_DEVICES = 3;
+
+
 /**
  * Start Google OAuth
  */
-authRouter.get(
-  "/google",
-  passport.authenticate("google", {
+authRouter.get("/google", passport.authenticate("google", {
     scope: ["profile", "email"],
     prompt: "login",
     accessType: "offline",
   })
 );
 
+
 /**
  * Google OAuth callback
  */
-authRouter.get(
-  "/google/callback",
-  passport.authenticate("google", {
+authRouter.get("/google/callback", passport.authenticate("google", {
     failureRedirect: `${env.FRONTEND_URL}/signin`,
   }),
-  (req, res, next) => {
+  
+  async (req, res, next) => {
 
-    const user = req.user;
-    if (!user) {
-      return next(new ApiError(401, "Authentication failed"));
-    }
-    req.session.regenerate(async err => {
-      if (err) {
-        return next(err);
+    try {
+      
+      const user = req.user;
+      if (!user) {
+        return next(new ApiError(401, "Authentication failed"));
       }
-      // store device session
-      await prisma.session.create({
-        data: {
+
+      // check active session before creating new one 
+      const activeSession = await prisma.session.findMany({
+        where: {
           userId: user.id,
-          sessionId: req.sessionID,
-          userAgent: req.headers["user-agent"],
-          ip: req.ip,
-          expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+          expiresAt: {
+            gt: new Date(),
+          },
         },
       });
-      
-      req.login(user, err => {
+
+      if (activeSession.length >= MAX_DEVICES) {
+        // block login
+        return res.redirect(`${env.FRONTEND_URL}/signin?error=device_limit`);
+      }
+
+      // regenrate session id
+      req.session.regenerate(async err => {
+  
         if (err) {
           return next(err);
         }
-        res.redirect(`${env.FRONTEND_URL}/explore`);
+        
+        req.login(user, async err => {
+
+          if (err) {
+            return next(err);
+          }
+    
+          // enforece device limtit and if reached then delete last session
+          // const existingSessions = await prisma.session.findMany({
+          //   where: {userId: user.id},
+          //   orderBy: {
+          //     createdAt: "asc",
+          //   },
+          // });
+    
+          // if (existingSessions.length >= MAX_DEVICES) {
+          //   const sessionsToDelete = existingSessions.slice(0, existingSessions.length - MAX_DEVICES + 1);
+    
+          //   await prisma.session.deleteMany({
+          //     where: {
+          //       id: {
+          //         in: sessionsToDelete.map(s => s.id),
+          //       },
+          //     },
+          //   });
+          // }
+    
+          // store device session
+          await prisma.session.create({
+            data: {
+              userId: user.id,
+              sessionId: req.sessionID,
+              userAgent: req.headers["user-agent"],
+              ip: req.ip,
+              expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+            },
+          });
+    
+          res.redirect(`${env.FRONTEND_URL}/explore`);
+        });
+
       });
-    });
+
+    } catch (err) {
+        next(err);
+    }
+    
   }
 );
 
@@ -76,16 +127,13 @@ authRouter.get(
 /**
  * Logout
  */
-authRouter.post(
-  "/logout",
-  requireAuth,
-  asyncHandler(async (req, res) => {
+authRouter.post("/logout", requireAuth, asyncHandler(async (req, res) => {
 
     // delete session when logout
     await prisma.session.deleteMany({
       where: {sessionId: req.sessionID},
     });
-    
+
     await new Promise<void>((resolve, reject) => {
       req.logout((err) => {
         if (err) return reject(err);
