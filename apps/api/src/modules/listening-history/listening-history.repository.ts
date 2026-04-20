17ce07d8dcad8c90b8bee1ae8e2e35e100da70c2
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-04-20 21:06:18 +0530

    feat(listening-history): added repository for listening history

---
+++ apps/api/src/modules/listening-history/listening-history.repository.ts
@@ -0,0 +1,67 @@
+import { prisma } from "@/lib/prisma";
+import { trackPlayInput } from "./schemas/track-play.schema";
+
+
+
+export type CreateHistoryInput = trackPlayInput & {
+    userId: string;
+};
+
+
+export class ListeningHistoryRepository {
+
+    async create(data: CreateHistoryInput) {
+        return prisma.listeningHistory.create({
+            data
+        });
+    };
+
+    async getRecentByUser(userId: string) {
+
+        const history = await prisma.listeningHistory.findMany({
+
+            where: {id: userId},
+            orderBy: {
+                playedAt: "desc"
+            },
+            take: 20,
+            select: {
+                playedAt: true,
+                song: {
+                    select: {
+                        id: true,
+                        title: true,
+                        duration: true,
+                        imageKey: true,
+                        album: {
+                            select: {
+                                id: true,
+                                title: true
+                            }
+                        },
+                        artists: {
+                            select: {
+                                artist: {
+                                    select: {
+                                        id: true,
+                                        name: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        return history.map(h => ({
+            id: h.song.id,
+            title: h.song.title,
+            duration: h.song.duration,
+            imageKey: h.song.imageKey,
+            playedAt: h.playedAt,
+            album: h.song.album,
+            artists: h.song.artists.map(a => a.artist)
+        }));
+    };
+}
